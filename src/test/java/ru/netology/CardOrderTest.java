fbe9920a5841fb41f4b484ef4841a5575f7283f0
Dmitriz1 <stud0000264980@study.utmn.ru>
2024-12-13 08:52:48 +0500

    last commit

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -21,28 +21,13 @@ public class CardOrderTest {
         $(By.cssSelector("[data-test-id='phone'] input")).setValue(formData.getPhone());
         $(By.cssSelector("[data-test-id='agreement']")).click();
         $(By.className("button")).click();
-        Duration.ofSeconds(2);
         $(By.className("notification__content"))
                 .shouldHave(text("Встреча успешно запланирована на " + formData.getDate()), Duration.ofSeconds(15))
                 .shouldBe(visible);
-    }
-
-    @Test
-    public void testRescheduleMeeting() {
-        FormData formData = DataGenerator.generateFormData();
-        open("http://localhost:9999");
-
-        $(By.cssSelector("[data-test-id='city'] input")).setValue(formData.getCity());
-        $(By.cssSelector("[data-test-id='date'] input")).setValue(formData.getDate());
-        $(By.cssSelector("[data-test-id='name'] input")).setValue(formData.getName());
-        $(By.cssSelector("[data-test-id='phone'] input")).setValue(formData.getPhone());
-        $(By.cssSelector("[data-test-id='agreement']")).click();
-        $(By.className("button")).click();
 
         $(By.cssSelector("[data-test-id='date'] input")).setValue(formData.getNewDate());
         $(By.className("button")).click();
         $(By.className("button_view_extra")).click();
-        Duration.ofSeconds(2);
         $(By.className("notification__content"))
                 .shouldHave(text("Встреча успешно запланирована на " + formData.getDate()), Duration.ofSeconds(15))
                 .shouldBe(visible);
